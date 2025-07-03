0252fca7a18775f94684b0de834c95376fb9c9c8
Sarathi <sarathi16072006@gmail.com>
2025-07-03 14:33:53 +0530

    Create 2053. Kth Distinct String in an Array .cpp

---
+++ Hashing/2053. Kth Distinct String in an Array .cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        unordered_map<string,int>map;
+        int count =0;
+        for(int i=0;i<arr.size();i++){
+            map[arr[i]]++;
+        }
+        for(int i=0;i<arr.size();i++){
+            if(map[arr[i]]==1){
+                count++;
+                if(count==k){
+                    return arr[i];
+                }
+            }
+        }
+        return "";
+        
+    }
+};
